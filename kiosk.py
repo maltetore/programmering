63ae4f50907a00fb12eee89b2e6b907a87cf35b3
maltetore <malte.klingstedt@gmail.com>
2025-09-18 13:51:58 +0200

    Update kiosk.py

--- kiosk.py
+++ kiosk.py
@@ -1,5 +1,17 @@
-print("what will it be, traveler?")
+gold = 200
+print("welcome to my shop. do you wish to buy or sell things?")
+if input() == "sell":
+    print("what do you want to sell?")
+    itemname = input()
+    print(f"how much do you want to sell {itemname} for?")
+    itemprice = input()
+    itemprice = int(itemprice)
 
+    print("deal.")
+    gold = gold + itemprice
+    print(f"you have {gold} gold. please buy something")
+
+print("do you what do you want to buy?")
 
 item = input()
 if item == "ice":
@@ -12,13 +24,24 @@ elif item == "candy":
     item = 10
 else:
     print("we dont have that")
+    quit()
 
 
 print("how many")
 ammount = input()
 ammount = int(ammount)
 
-print(f"that is worth {item * ammount}")
-
+print(f"that will be {item * ammount}")
+print("pay")
+if input() == "yes":
+    gold = (item * ammount) - gold
+    print("thanks")
+elif input() == "no":
+    print("no items for you")
+    quit()
+else:
+    print("in english, please.")
+print(gold)
+quit()
 
 
